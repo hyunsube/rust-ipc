033c94937ce2a52dcad8e832b701e28c9f48944b
hyunsube <gustjqe@gmail.com>
2024-01-04 21:38:23 +0900

    add comment

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
 use zbus::{Connection, dbus_interface, Result};
 
 
-struct Greeter;
+struct DataHandler; // Name for server
 
-#[dbus_interface(name = "org.zbus.MyGreeter1")]
-impl Greeter {
-    async fn say_hello(&self, name: &str) -> String {
-        format!("Hello {}!", name)
+#[dbus_interface(name = "org.zbus.DataHandler")]
+impl DataHandler {
+    async fn say_hello(&self, name: &str) -> String { // We will call this method as PascalCase from snake_case --> SayHello
+        format!("Hello {}!", name) // Handling part
     }
 }
 
-// Although we use `async-std` here, you can use any async runtime of choice.
 #[async_std::main]
 async fn main() -> Result<()> {
     let connection = Connection::session().await?;
     // setup the server
     connection
         .object_server()
-        .at("/org/zbus/MyGreeter", Greeter)
+        .at("/org/zbus/DataHandler", DataHandler)
         .await?;
     // before requesting the name
     connection
-        .request_name("org.zbus.MyGreeter")
+        .request_name("org.zbus.DataHandler")
         .await?;
 
     loop {
         // do something else, wait forever or timeout here:
         // handling D-Bus messages is done in the background
-        std::future::pending::<()>().await;
     }
 }
